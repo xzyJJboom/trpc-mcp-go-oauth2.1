180c833f12a3a824365073a037d87d8cdabbe67d
Lemme_cook <1356088661@qq.com>
2025-08-13 09:11:09 +0800

    update,错误码更新

--- internal/errors/auth.go
+++ internal/errors/auth.go
@@ -1,20 +1,48 @@
 package errors
 
+import (
+	"errors"
+)
+
+type OAuthError error
+
 // OAuthErrorResponse OAuth 2.1草案标准错误响应。
 type OAuthErrorResponse struct {
-	Error            string  `json:"error"`
-	ErrorDescription *string `json:"error_description,omitempty"`
-	ErrorURI         *string `json:"error_uri,omitempty"`
+	Error            string `json:"error"`
+	ErrorDescription string `json:"error_description,omitempty"`
+	ErrorURI         string `json:"error_uri,omitempty"`
 }
 
-// ToResponseObject 转换为OAuthErrorResponse。
-func (e OAuthError) ToResponseObject() OAuthErrorResponse {
+var (
+	// OAuth Errors
+	ErrInvalidRequest          OAuthError = errors.New("invalid request")
+	ErrInvalidClient           OAuthError = errors.New("invalid client")
+	ErrInvalidGrant            OAuthError = errors.New("invalid grant")
+	ErrUnauthorizedClient      OAuthError = errors.New("unauthorized client")
+	ErrUnsupportedGrantType    OAuthError = errors.New("unsupported grant type")
+	ErrInvalidScope            OAuthError = errors.New("invalid scope")
+	ErrAccessDenied            OAuthError = errors.New("access denied")
+	ErrServerError             OAuthError = errors.New("server error")
+	ErrTemporarilyUnavailable  OAuthError = errors.New("temporarily unavailable")
+	ErrUnsupportedResponseType OAuthError = errors.New("unsupported response type")
+	ErrUnsupportedTokenType    OAuthError = errors.New("unsupported token type")
+	ErrInvalidToken            OAuthError = errors.New("invalid token")
+	ErrMethodNotAllowed        OAuthError = errors.New("method not allowed")
+	ErrTooManyRequests         OAuthError = errors.New("too many requests")
+	ErrInvalidClientMetadata   OAuthError = errors.New("invalid client metadata")
+	ErrInsufficientScope       OAuthError = errors.New("insufficient scope")
+)
+
+// NewOAuthErrorResponse creates a new OAuthErrorResponse
+func NewOAuthErrorResponse(errCode OAuthError, message string, uri string) OAuthErrorResponse {
 	resp := OAuthErrorResponse{
-		Error:            e.Code,
-		ErrorDescription: &e.Message,
+		Error: errCode.Error(),
 	}
-	if e.URI != nil {
-		resp.ErrorURI = e.URI
+	if uri != "" {
+		resp.ErrorURI = uri
+	}
+	if message != "" {
+		resp.ErrorDescription = message
 	}
 	return resp
 }
